c9a42cf88a3d0886cfb30f54ce98a1fcb1e50c55
Benndora1 <Benndora2@gmail.com>
2022-03-15 09:57:33 +0300

    admin

--- staff/urls.py
+++ staff/urls.py
@@ -5,4 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('staffclick/', views.staffclick_view, name='staffclick'),
     path('staffsignup/', views.staffsignup_view, name='staffsignup'),
+    path('stafflogin', LoginView.as_view(template_name='vehicle/adminlogin.html'),name='stafflogin'),
+    path('staffdashboard/', views.staff_dashboard_view, name='staffdashboard'),
 ]
--- vehicle/views.py
+++ vehicle/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse
+from . import forms, models
+from django.contrib.auth.decorators import login_required,user_passes_test
+from django.contrib.auth.models import Group
+from django.http import HttpResponseRedirect
+from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def home_view(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('afterlogin')
+    return render(request, 'vehicle/index.html')
+
+def is_staff(user):
+    return user.groups.filter(name='STAFF').exists()
+
+def afterlogin(request):
+    if is_staff(request.user):
+        return redirect('staff/staffdashboard')
+    else:
+        return redirect('admin-dashboard')
+
+def adminclick_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return HttpResponseRedirect('adminlogin')
+
+@login_required(login_url='adminlogin')
+def admin_dashboard_view(request):
+    dict= {
+        'staff':model.staff.objects.get(user=request.user)
+    }
+    return render(request, 'vehicle/admin_dashboard.html', context=dict)
+
+@login_required(login_url='adminlogin')
+def adminlogin_view(request):
+    staffs=model.staff.objects.all()
+    return render(request, 'vehicle/admin_view_staffs.html', {'staffs':staffs})

5249f45ade5531734149d6614013801513532e46
Benndora1 <Benndora2@gmail.com>
2022-03-15 12:49:36 +0300

    vehicle

--- vehicle/views.py
+++ vehicle/views.py
@@ -38,3 +38,43 @@ def admin_dashboard_view(request):
 def adminlogin_view(request):
     staffs=model.staff.objects.all()
     return render(request, 'vehicle/admin_view_staffs.html', {'staffs':staffs})
+
+
+@login_required(login_url='adminlogin')
+def update_staffs_view(request):
+    staffs=CMODEL.Staff.objects.get(id=pk)
+    user=CMODEL.User.objects.get(id=staff.user_id)
+    userForm=CFORM.StaffUserForm(instance=user)
+    staffForm=CFORM.StaffForm(request.Files, instance=staff)
+    mydict={'userForm':userForm, 'staffForm':staffForm}
+    if request.method=='POST':
+        userForm=CFORM.StaffUserForm(request.POST, instance=user)
+        staffForm=CFORM.StaffForm(request.POST, request.FILES, instance=staff)
+        if userForm.is_valid() and staffForm.is_valid():
+            user=userForm.save()
+            user.set_password(user.password)
+            user.save()
+            staffForm.save()
+            return HttpResponseRedirect('adminlogin')
+    return render(request, 'vehicle/admin_update_staffs.html', context=mydict)
+
+
+
+@login_required(login_url='adminlogin')
+def delete_staffs_view(request, pk):
+    staff=CMODEL.Staff.objects.get(id=pk)
+    user=CMODEL.User.objects.get(id=staff.user_id)
+    user.delete()
+    staff.delete()
+    return HttpResponseRedirect('/admin-view-staffs')
+
+def admin_add_vehicle_view(request):
+    vehicleForm=forms.VehicleForm()
+
+    if request.method=='POST':
+        vehicleForm=forms.VehicleForm(request.POST)
+        if vehicleForm.is_valid():
+            vehicle=vehicleForm.save()
+            vehicle.save()
+            return redirect ('admin-view-vehicles')
+    return render(request, 'vehicle/admin_add_vehicle.html', {'vehicleForm':vehicleForm})
